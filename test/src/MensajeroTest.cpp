d58e4ac43f9a980ff2d8b134d0be2be082364f21
gemtugnarelli <mariano.tugnarelli@grupoesfera.com.ar>
2015-06-10 19:56:46 -0300

    Proyecto con las pruebas unitarias.

---
+++ test/src/MensajeroTest.cpp
@@ -0,0 +1,13 @@
+
+#include "gtest/gtest.h"
+#include "Mensajero.h"
+
+/* hack/truco para poder correr las pruebas sin copiar los fuentes */
+#include "Mensajero.cpp"
+
+
+
+TEST(MensajeroTest, crear) {
+
+
+}
